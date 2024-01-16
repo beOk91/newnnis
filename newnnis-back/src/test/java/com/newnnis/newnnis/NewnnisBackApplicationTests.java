32eef9c8e644d64b7b94bab06bda7b7f197e2d5f
beOk91 <be_ok91@naver.com>
2024-01-16 21:09:27 +0900

    commit

---
+++ newnnis-back/src/test/java/com/newnnis/newnnis/NewnnisBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newnnis.newnnis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewnnisBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
